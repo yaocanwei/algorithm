3feac56068b6fbc781f25669b81ec90efe3f8c2f
yaocanwei <1248014498@qq.com>
2016-10-30 16:45:10 +0800

    add bubble sort

---
+++ algorithm.rb
@@ -0,0 +1,26 @@
+require 'pry-rails'
+require 'pry-nav'
+
+class Algorithm
+
+  #冒泡排序
+  def bubble_sort(array)
+
+    n = array.length
+
+    loop do
+      swapped = false
+
+      (n-1).times do |i|
+        if array[i] >= array[i+1]
+          array[i], array[i+1]=array[i+1], array[i]
+          swapped = true
+        end
+      end
+
+      break if not swapped
+    end
+    array
+  end
+
+end
